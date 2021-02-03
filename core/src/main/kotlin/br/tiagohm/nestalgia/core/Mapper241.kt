8c2588e76d86eb8a3e4fe95cacd5da2b27ad010c
tiagohm <tiago.henrique.cco@gmail.com>
2021-02-03 17:48:36 -0300

    [core]: Implementar Mapper 241

---
+++ core/src/main/kotlin/br/tiagohm/nestalgia/core/Mapper241.kt
@@ -0,0 +1,20 @@
+package br.tiagohm.nestalgia.core
+
+// https://wiki.nesdev.com/w/index.php/INES_Mapper_241
+
+@ExperimentalUnsignedTypes
+class Mapper241 : Mapper() {
+
+    override val prgPageSize = 0x8000U
+
+    override val chrPageSize = 0x2000U
+
+    override fun init() {
+        selectPrgPage(0U, 0U)
+        selectChrPage(0U, 0U)
+    }
+
+    override fun writeRegister(addr: UShort, value: UByte) {
+        selectPrgPage(0U, value.toUShort())
+    }
+}
