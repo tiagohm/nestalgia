8b72defb6c59a13201bd454643c7623e06d54339
tiagohm <tiago.henrique.cco@gmail.com>
2023-06-10 23:56:50 -0300

    [core]: New mapper: 39

--- core/src/main/kotlin/br/tiagohm/nestalgia/core/Mapper.kt
+++ core/src/main/kotlin/br/tiagohm/nestalgia/core/Mapper.kt
@@ -1007,6 +1007,7 @@ abstract class Mapper : Resetable, Battery, Peekable, MemoryHandler, Closeable, 
                 36 -> Txc22000()
                 37 -> Mapper037()
                 38 -> UnlPci556()
+                39 -> Mapper039()
                 40 -> Mapper040()
                 44 -> Mapper044()
                 45 -> Mapper045()
