92627a87b429614671a1abcf70e8dbdfcb94caed
tiagohm <tiago.henrique.cco@gmail.com>
2023-06-14 17:29:46 -0300

    [core]: New mapper: Sachen9602

--- core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
+++ core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
@@ -155,6 +155,7 @@ object MapperFactory {
             286 -> Bs5(console)
             328 -> Rt01(console)
             346 -> Kaiser7012(console)
+            513 -> Sachen9602(console)
             FDS_MAPPER_ID -> Fds(console)
             32770 -> Gs2013(console)
             else -> {
---
+++ core/src/main/kotlin/br/tiagohm/nestalgia/core/Sachen9602.kt
@@ -0,0 +1,43 @@
+package br.tiagohm.nestalgia.core
+
+class Sachen9602(console: Console) : MMC3(console) {
+
+    override val chrRamSize = 0x8000
+
+    override val isForceChrBattery = true
+
+    private val regs = IntArray(2)
+
+    override fun selectPrgPage(slot: Int, page: Int, memoryType: PrgMemoryType) {
+        super.selectPrgPage(slot, (page and 0x3F) or (regs[1] shl 6), memoryType)
+        super.selectPrgPage(if (prgMode != 0) 0 else 2, 0x3E, memoryType)
+        super.selectPrgPage(3, 0x3F, memoryType)
+    }
+
+    override fun writeRegister(addr: Int, value: Int) {
+        var newValue = value
+
+        when (addr and 0xE001) {
+            0x8000 -> regs[0] = value
+            0x8001 -> if (regs[0] and 0x07 < 6) {
+                regs[1] = value shr 6
+                newValue = value and 0x1F
+                updatePrgMapping()
+            }
+        }
+
+        super.writeRegister(addr, newValue)
+    }
+
+    override fun saveState(s: Snapshot) {
+        super.saveState(s)
+
+        s.write("regs", regs)
+    }
+
+    override fun restoreState(s: Snapshot) {
+        super.restoreState(s)
+
+        s.readIntArray("regs", regs)
+    }
+}
