f1f58e709a1352858b229b10361cbb2cb4f08a7c
tiagohm <tiago.henrique.cco@gmail.com>
2023-06-21 13:38:19 -0300

    [core]: New mapper: 201

--- core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
+++ core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
@@ -147,6 +147,7 @@ object MapperFactory {
             198 -> Mapper198(console)
             199 -> Mapper199(console)
             200 -> Mapper200(console)
+            201 -> NovelDiamond(console)
             202 -> Mapper202(console)
             203 -> Mapper203(console)
             204 -> Mapper204(console)
