2e9cf5efaed2d44a6589c2cf11cac12a8fdbef05
tiagohm <tiago.henrique.cco@gmail.com>
2023-06-25 05:19:52 -0300

    [core]: New mapper: BMC-WS

--- core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
+++ core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
@@ -251,6 +251,7 @@ object MapperFactory {
             313 -> ResetTxrom(console)
             325 -> MaliSB(console)
             328 -> Rt01(console)
+            332 -> Super40in1Ws(console)
             346 -> Kaiser7012(console)
             348 -> Bmc830118C(console)
             513 -> Sachen9602(console)
