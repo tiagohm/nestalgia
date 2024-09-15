d540d6e6f64a862a626bb03892638ad4dba781ed
tiagohm <tiago.henrique.cco@gmail.com>
2024-09-15 16:22:15 -0300

    [core]: New mapper: UNL-Coolboy

--- core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
+++ core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
@@ -253,6 +253,7 @@ object MapperFactory {
             263 -> MMC3Kof97(console)
             264 -> Yoko(console)
             265 -> T262(console)
+            268 -> MMC3Coolboy(console)
             283 -> Gs2004(console)
             284 -> UnlDripGame(console)
             285 -> A65AS(console)
