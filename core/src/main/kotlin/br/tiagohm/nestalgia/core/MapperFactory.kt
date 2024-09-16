c112a9b61751e2ab17022bb777cc7c9830204e99
tiagohm <tiago.henrique.cco@gmail.com>
2024-09-16 12:10:55 -0300

    [core]: New mapper: BMC-HP898F

--- core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
+++ core/src/main/kotlin/br/tiagohm/nestalgia/core/MapperFactory.kt
@@ -275,6 +275,7 @@ object MapperFactory {
             309 -> Lh51(console)
             313 -> ResetTxrom(console)
             314 -> Bmc64in1NoRepeat(console)
+            319 -> Hp898f(console)
             325 -> MaliSB(console)
             328 -> Rt01(console)
             329 -> Edu2000(console)
